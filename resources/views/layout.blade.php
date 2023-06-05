d1e5f5e40eb0e87069ee28918709fdeed6e7d14f
Sanae Bouazzah <sanaebouazzah2000@gmail.com>
2023-06-05 22:40:35 +0200

    Student-Management

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -9,6 +9,7 @@
 <body>
   <div class="container">
      <div class="row">
+      <div class="col md-12"></div>
       <nav class="navbar navbar-expand-lg navbar-light bg-light">
         <a class="navbar-brand" href="#">Navbar</a>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
