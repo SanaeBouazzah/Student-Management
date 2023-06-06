5dc8a5898bd1015008e6d30297e40ef192f1319b
Sanae Bouazzah <sanaebouazzah2000@gmail.com>
2023-06-07 00:48:53 +0200

    Student-Management

--- resources/views/students/show.blade.php
+++ resources/views/students/show.blade.php
@@ -4,5 +4,12 @@
 @endsection
 @section('content')
    <h2> {{$student->name}}</h2>
-   hi
+   <div class="card">
+    <h5 class="card-header">Profile</h5>
+    <div class="card-body">
+      <h5 class="card-title">Special title treatment</h5>
+      <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+      <a href="#" class="btn btn-primary">Go somewhere</a>
+    </div>
+  </div>
 @endsection
