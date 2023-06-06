1455a9f1a0bd54f2bd4aed11b7908f49c40f8622
Sanae Bouazzah <sanaebouazzah2000@gmail.com>
2023-06-07 01:15:10 +0200

    Student-Management

--- routes/web.php
+++ routes/web.php
@@ -2,12 +2,12 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\StudentController;
-
+use App\Http\Controllers\TeacherController;
 
 Route::get('/', function(){return view('layouts.layout');})->name('home');
 
 Route::resource('/students', StudentController::class);
-Route::resource('/teachers', StudentController::class);
+Route::resource('/teachers', TeacherController::class);
 
 
 
