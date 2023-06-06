130bee2ea8ba10683b0c6bb96227414864efbbdd
Sanae Bouazzah <sanaebouazzah2000@gmail.com>
2023-06-07 01:05:32 +0200

    Student-Management

--- app/Http/Controllers/TeacherController.php
+++ app/Http/Controllers/TeacherController.php
@@ -2,63 +2,56 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Teacher;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\View;
 
-class TeacherController extends Controller
+class teacherController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     */
     public function index()
     {
-        //
+        $teachers = Teacher::all();
+        return view('teachers.index', compact('teachers'));
     }
-
-    /**
-     * Show the form for creating a new resource.
-     */
     public function create()
     {
-        //
+        return view('teachers.create');
     }
-
-    /**
-     * Store a newly created resource in storage.
-     */
     public function store(Request $request)
     {
-        //
+       //validation
+        $data = $request->validate([
+          'name' => 'required',
+          'address' => 'required',
+          'mobile' => 'required'
+        ]);
+        //insertion
+        Teacher::create($data);
+        return redirect()->route('teachers.index')->with('success', 'You added teacher successfuly.');
     }
-
-    /**
-     * Display the specified resource.
-     */
-    public function show(string $id)
+    public function show(Teacher $teacher)
     {
-        //
+      return view('teachers.show')->with('teacher',$teacher);
     }
-
-    /**
-     * Show the form for editing the specified resource.
-     */
-    public function edit(string $id)
+    public function edit(Teacher $teacher)
     {
-        //
+        return view('teachers.edit')->with('teacher', $teacher);
     }
-
-    /**
-     * Update the specified resource in storage.
-     */
-    public function update(Request $request, string $id)
+    public function update(Request $request, Teacher $teacher)
     {
-        //
+       //validation
+       $data = $request->validate([
+        'name' => 'required',
+        'address' => 'required',
+        'mobile' => 'required'
+      ]);
+      //insertion
+      $teacher->fill($data)->save($data);
+      return redirect()->route('teachers.index')->with('success', 'You updated teacher successfuly.');
     }
-
-    /**
-     * Remove the specified resource from storage.
-     */
-    public function destroy(string $id)
+    public function destroy(Teacher $teacher)
     {
-        //
+        $teacher->delete();
+        return redirect()->route('teachers.index')->with('success', 'you have been deleted teacher successfuly.');
     }
 }
