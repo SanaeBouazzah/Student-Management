87e6ba7f04db468c88730d9766b0f3618fdb60ed
Sanae Bouazzah <sanaebouazzah2000@gmail.com>
2023-06-07 01:14:04 +0200

    Student-Management

--- routes/web.php
+++ routes/web.php
@@ -5,13 +5,9 @@ use App\Http\Controllers\StudentController;
 
 
 Route::get('/', function(){return view('layouts.layout');})->name('home');
-Route::get('/students', [StudentController::class, 'index'])->name('students.index');
-Route::get('/students/create', [StudentController::class, 'create'])->name('students.create');
-Route::post('/students', [StudentController::class, 'store'])->name('students.store');
-Route::get('/students/{student}', [StudentController::class, 'show'])->name('students.show');
-Route::get('/students/{student}/edit', [StudentController::class, 'edit'])->name('students.edit');
-Route::put('/students/{student}', [StudentController::class, 'update'])->name('students.update');
-Route::delete('/students/{student}', [StudentController::class, 'destroy'])->name('students.destroy');
+
+Route::resource('/students', StudentController::class);
+Route::resource('/teachers', StudentController::class);
 
 
 
