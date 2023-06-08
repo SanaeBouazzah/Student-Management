506b3ce5ff2ecab3a0fd7842cbb4bb975d66da63
Sanae Bouazzah <sanaebouazzah2000@gmail.com>
2023-06-08 14:49:31 +0200

    Student-Management

--- app/Http/Controllers/PaymentController.php
+++ app/Http/Controllers/PaymentController.php
@@ -6,9 +6,6 @@ use Illuminate\Http\Request;
 
 class PaymentController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     */
     public function index()
     {
         //
