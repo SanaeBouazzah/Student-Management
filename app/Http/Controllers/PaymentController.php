677f2c8306db54954ca84a6b752bcd74b3ede5a2
Sanae Bouazzah <sanaebouazzah2000@gmail.com>
2023-06-08 15:03:48 +0200

    Student-Management

--- app/Http/Controllers/PaymentController.php
+++ app/Http/Controllers/PaymentController.php
@@ -10,7 +10,8 @@ class PaymentController extends Controller
 {
     public function index()
     {
-        //
+        $payments = Payment::all();
+        return view('payments.index', compact('payments'));
     }
     public function create()
     {
