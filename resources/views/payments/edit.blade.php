a8140ba2d7096b22c5f237401cf020483b498e48
Sanae Bouazzah <sanaebouazzah2000@gmail.com>
2023-06-09 12:11:00 +0200

    Student-Management

--- resources/views/payments/edit.blade.php
+++ resources/views/payments/edit.blade.php
@@ -9,8 +9,7 @@
   @csrf
   <div class="form-group my-3 mx-5">
     <label for="enrollment_id" class="mb-2">Enrollment No</label><br/>
-    <select name="enrollment_id" id="enrollment_id" class="form-select" 
-    aria-label="Default select example" >
+    <select name="enrollment_id" id="enrollment_id" class="form-select" aria-label="Default select example" >
       @foreach ($enrollments as $id => $enroll_no)
         <option value="{{$id}}">{{$enroll_no}}</option>
       @endforeach
@@ -18,11 +17,11 @@
   </div>
   <div class="form-group my-3 mx-5">
     <label for="paid_date" class="mb-2">Paid Date</label>
-    <input type="text" class="form-control" id="paid_date" name="paid_date" placeholder="Enter your paid date">
+    <input type="text" class="form-control" id="paid_date" name="paid_date" placeholder="Enter your paid date" value="{{$payment->paid_date}}">
   </div>
   <div class="form-group my-3 mx-5">
     <label for="amount" class="mb-2">Amount</label>
-    <input type="text" class="form-control" id="amount" name="amount" placeholder="Enter your amount">
+    <input type="text" class="form-control" id="amount" name="amount" placeholder="Enter your amount" value="{{$payment->amount}}">
   </div>
   <div class="mx-5 my-4">
     <input type="submit" class="btn btn-primary form-control " value="submit">
