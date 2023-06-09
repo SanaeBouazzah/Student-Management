20b6d661f6a1446cc42576bf4876f2205bd285dc
Sanae Bouazzah <sanaebouazzah2000@gmail.com>
2023-06-09 11:58:42 +0200

    Student-Management

--- resources/views/payments/edit.blade.php
+++ resources/views/payments/edit.blade.php
@@ -5,20 +5,23 @@
 @section('content')
 <div class="mt-4">
   <h2 class="mb-4">Edit a Payment :</h2>
-<form style="width:80%; margin:auto;" class="px-4 py-5 shadow-lg" action="{{route('payments.update', $payment)}}" method="POST">
+<form style="width:80%; margin:auto;" class="px-4 py-5 shadow-lg" action="{{route('payments.store')}}" method="POST">
   @csrf
-  @method('PUT')
   <div class="form-group my-3 mx-5">
-    <label for="name" class="mb-2">Name</label>
-    <input type="name" class="form-control" id="name" name="name" placeholder="Enter your name" value="{{$payment->name}}">
+    <label for="enrollment_id" class="mb-2">Enrollment No</label><br/>
+    <select name="enrollment_id" id="enrollment_id" class="form-select" aria-label="Default select example">
+      @foreach ($enrollments as $id => $enroll_no)
+        <option value="{{$id}}">{{$enroll_no}}</option>
+      @endforeach
+    </select>
   </div>
   <div class="form-group my-3 mx-5">
-    <label for="address" class="mb-2">Address</label>
-    <input type="address" class="form-control" id="address" name="address" placeholder="Enter your address" value="{{$payment->address}}">
+    <label for="paid_date" class="mb-2">Paid Date</label>
+    <input type="text" class="form-control" id="paid_date" name="paid_date" placeholder="Enter your paid date">
   </div>
-  <div class="form-group mx-5 my-3">
-    <label for="mobile" class="mb-2">mobile</label>
-    <input type="mobile" class="form-control" id="mobile" name="mobile" placeholder="Enter your mobile" value="{{$payment->mobile}}">
+  <div class="form-group my-3 mx-5">
+    <label for="amount" class="mb-2">Amount</label>
+    <input type="text" class="form-control" id="amount" name="amount" placeholder="Enter your amount">
   </div>
   <div class="mx-5 my-4">
     <input type="submit" class="btn btn-primary form-control " value="submit">
