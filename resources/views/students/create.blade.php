d2e548f69afa18241361849608ca63aeceee53fd
Sanae Bouazzah <sanaebouazzah2000@gmail.com>
2023-06-10 23:44:54 +0200

    Student-Management

--- resources/views/students/create.blade.php
+++ resources/views/students/create.blade.php
@@ -23,7 +23,7 @@
     <input type="submit" class="btn btn-primary form-control " value="submit">
   </div>
   <div class="form-group mx-5 my-3">
-    <label for="password"></label>
+    <label for="password">Password</label>
   </div>
 </form>
 <a href="{{route('students.index')}}" class="btn btn-dark rounded-0 px-4 my-4">Back</a>
