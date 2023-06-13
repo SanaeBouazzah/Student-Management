fa2ee8c3df7ab78632feb587d8ccb6661cf03873
Sanae Bouazzah <sanaebouazzah2000@gmail.com>
2023-06-14 00:55:29 +0200

    Student-Management

--- resources/views/students/show.blade.php
+++ resources/views/students/show.blade.php
@@ -7,7 +7,7 @@
   <div class="col-lg-4">
     <div class="card mb-4">
       <div class="card-body text-center">
-        <img src="https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-chat/ava3.webp" alt="avatar"
+        <img src="{{asset('storage/'. $student->image)}}" alt="avatar"
           class="rounded-circle img-fluid" style="width: 150px;">
         <h5 class="my-3">John Smith</h5>
         <p class="text-muted mb-1">Full Stack Developer</p>
