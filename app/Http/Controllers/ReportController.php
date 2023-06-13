91506c73bf64da9c7e512c7e53b9e2f76b0b8748
Sanae Bouazzah <sanaebouazzah2000@gmail.com>
2023-06-13 19:17:36 +0200

    Student-Management

--- app/Http/Controllers/ReportController.php
+++ app/Http/Controllers/ReportController.php
@@ -2,10 +2,15 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Controllers\ReportController;
 use Illuminate\Http\Request;
+use App;
+use Auth;
+use App\Models\Payment;
 
 class ReportController extends Controller
 {
   $payment = Payment::find($pid);
   $pdf = App::make('dompdf.wrapper');
+  $print = "<div>;"
 }  
