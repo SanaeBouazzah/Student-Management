d75abc0b6076e6997bed3c7adbb0514ec8082500
Sanae Bouazzah <sanaebouazzah2000@gmail.com>
2023-06-13 23:48:11 +0200

    Student-Management

--- resources/views/students/create.blade.php
+++ resources/views/students/create.blade.php
@@ -21,7 +21,7 @@
   </div>
   <div class="form-group mx-5 my-3">
     <label for="image" class="mb-2">mobile</label>
-    <input type="file" class="form-control" id="mobile" name="mobile" placeholder="Enter your mobile">
+    <input type="file" class="form-control" id="image" name="image">
   </div>
   <div class="mx-5 my-4">
     <input type="submit" class="btn btn-primary form-control " value="submit">
