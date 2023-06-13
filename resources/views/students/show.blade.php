b3123bee91281a086dafc66b0eb0507ddd260705
Sanae Bouazzah <sanaebouazzah2000@gmail.com>
2023-06-13 23:46:05 +0200

    Student-Management

--- resources/views/students/show.blade.php
+++ resources/views/students/show.blade.php
@@ -4,16 +4,18 @@
 @endsection
 @section('content')
    <h2> {{$student->name}}</h2>
-   <div class="image">
-    <img src="{{$student->image}}" alt="">
-   </div>
-   <div class="card">
-    <h5 class="card-header">Profile</h5>
-    <div class="card-body">
-      <h5 class="card-title">{{$student->name}}</h5>
-      <p class="card-text">{{$student->address}}</p>
-      <p>{{$student->mobile}}</p>
-      <a href="{{route('students.index')}}" class="btn btn-primary">Back</a>
+  <div class="d-flex">
+    <div class="image">
+      <img src="{{$student->image}}" alt="">
+     </div>
+     <div class="card">
+      <h5 class="card-header">Profile</h5>
+      <div class="card-body">
+        <h5 class="card-title">{{$student->name}}</h5>
+        <p class="card-text">{{$student->address}}</p>
+        <p>{{$student->mobile}}</p>
+        <a href="{{route('students.index')}}" class="btn btn-primary">Back</a>
+      </div>
     </div>
   </div>
 @endsection
