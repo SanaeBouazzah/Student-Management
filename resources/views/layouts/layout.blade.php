57e2e12541d93d286bb5ffa5da6f8bd97684b492
Sanae Bouazzah <sanaebouazzah2000@gmail.com>
2023-06-13 20:22:57 +0200

    Student-Mangement

--- resources/views/layouts/layout.blade.php
+++ resources/views/layouts/layout.blade.php
@@ -3,7 +3,7 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
-  <title>Student Management @yield('title')</title>
+  <title>Student Management --r @yield('title')</title>
   <link rel="stylesheet" href="{{asset('css/style.css')}}">
   <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css">
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/js/bootstrap.min.js"></script>
