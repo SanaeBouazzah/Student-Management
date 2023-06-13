45c273c4144871c1b505e0f520f7e93a55a3b31d
Sanae Bouazzah <sanaebouazzah2000@gmail.com>
2023-06-14 00:50:51 +0200

    Student-Management

--- resources/views/students/show.blade.php
+++ resources/views/students/show.blade.php
@@ -4,12 +4,12 @@
 @endsection
 @section('content')
    <h2> {{$student->name}}</h2>
-  <div class="d-flex justify-content-start " style="width:60%;">
-    <div class="image">
+  <div class="d-flex justify-content-start my-3" style="width:60%;">
+    <div class="image px-2">
       <img src="{{asset('storage/' . $student->image)}}" alt="" 
       style="object-fit:cover" width="140px" height="140px">
     </div>
-     <div class="rep">
+     <div class="rep px-2">
       <h5 class="card-header">Profile</h5>
       <div class="card-body">
         <h5 class="card-title">{{$student->name}}</h5>
