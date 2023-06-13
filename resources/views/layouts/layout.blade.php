f3f3f4fb5855d90a7598a682cfcb9ad0e8f6b86c
Sanae Bouazzah <sanaebouazzah2000@gmail.com>
2023-06-13 18:55:13 +0200

    Student-Management

--- resources/views/layouts/layout.blade.php
+++ resources/views/layouts/layout.blade.php
@@ -5,8 +5,6 @@
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
   <title>Student Management | @yield('title')</title>
   <link rel="stylesheet" href="{{asset('css/style.css')}}">
-  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
-  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
   <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css">
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/js/bootstrap.min.js"></script>
 </head>
