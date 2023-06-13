b718769a33177a95af1e599a32babfa382c01d25
Sanae Bouazzah <sanaebouazzah2000@gmail.com>
2023-06-13 23:43:37 +0200

    Student-Management

--- database/migrations/2023_06_05_214116_create_students_table.php
+++ database/migrations/2023_06_05_214116_create_students_table.php
@@ -13,6 +13,7 @@ return new class extends Migration
     {
         Schema::create('students', function (Blueprint $table) {
             $table->id();
+            $table->string('image');
             $table->string('name');
             $table->string('address');
             $table->string('mobile');
