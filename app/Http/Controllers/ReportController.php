a5192cc9477e5261110a9c3c6e7c02593567d740
Sanae Bouazzah <sanaebouazzah2000@gmail.com>
2023-06-13 19:29:41 +0200

    Student-Management

--- app/Http/Controllers/ReportController.php
+++ app/Http/Controllers/ReportController.php
@@ -2,7 +2,6 @@
 
 namespace App\Http\Controllers;
 
-use App\Http\Controllers\ReportController;
 use Illuminate\Http\Request;
 use App;
 use Auth;
@@ -13,8 +12,28 @@ class ReportController extends Controller
   public function report1($pid){
     $payment = Payment::find($pid);
     $pdf = App::make('dompdf.wrapper');
-    $print = "<div style='margin20px, padding:20px'>;"
+    $print = "<div style='margin20px, padding:20px'>";
     $print .= "<h1>Payment Receipt</h1>";
-    $print .= "";
+    $print .= "<hr/>";
+    $print .= "<p>Receipt No: <b>" . $pid . "</b> </p>";
+
+    $print .= "<tr>";
+    $print .= "<td></td>";
+    $print .= "<td></td>";
+    $print .= "</tr>";
+    $print .= "<tr>";
+    $print .= "<td></td>";
+    $print .= "<td></td>";
+    $print .= "</tr>";
+    $print .= "<tr>";
+    $print .= "<td><h3>" . $payment->enrollment->batch->name . "</h3></td>";
+    $print .= "<td></td>";
+    $print .= "</tr>";
+    $print .= "</table>";
+    $print .= "<hr/>";
+    $print .= "<span>Printed Date : <b>" . date('Y-m-d') . "</b></span>";
+    $print .= "</div>";
+    $pdf->loadHTML($print);
+    return $pdf->stream();
   }
 }  
